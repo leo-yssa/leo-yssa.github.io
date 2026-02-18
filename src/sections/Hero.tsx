679e51a2ad96f3482e49b73f37eeda0c89e22daa
leo <leo@leoui-MacBookPro.local>
2026-02-19 00:36:08 +0900

    [Fix] 빌드 에러 수정

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,8 @@
 import { FaChevronDown } from 'react-icons/fa';
-import { useLanguage } from '../context/LanguageContext';
+
 import styles from './Hero.module.css';
 
 const Hero = () => {
-    const { t } = useLanguage();
 
     const scrollToAbout = () => {
         document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' });
